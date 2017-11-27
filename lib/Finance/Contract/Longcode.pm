629a4673e5b837fba4b504585c9131b86448255f
Shahrizal Shaari <shahrizal@binary.com>
2017-11-27 07:25:45 +0000

    chg shortcode to params

--- lib/Finance/Contract/Longcode.pm
+++ lib/Finance/Contract/Longcode.pm
@@ -150,7 +150,7 @@ sub shortcode_to_parameters {
 
     my ($bet_type, $underlying_symbol, $payout, $date_start, $date_expiry, $barrier, $barrier2, $prediction, $fixed_expiry, $tick_expiry,
         $how_many_ticks, $forward_start, $binaryico_per_token_bid_price,
-        $binaryico_number_of_tokens, $binaryico_deposit_percentage);
+        $binaryico_number_of_tokens, $binaryico_deposit_percentage, $contract_multiplier);
 
     my ($initial_bet_type) = split /_/, $shortcode;
 
@@ -162,7 +162,7 @@ sub shortcode_to_parameters {
 
     return $legacy_params if (not exists Finance::Contract::Category::get_all_contract_types()->{$initial_bet_type} or $shortcode =~ /_\d+H\d+/);
 
-    if ($shortcode =~ /^([^_]+)_([\w\d]+)_(\d*\.?\d*)_(\d+)(?<start_cond>F?)_(\d+)(?<expiry_cond>[FT]?)_(S?-?\d+P?)_(S?-?\d+P?)$/)
+    if ($shortcode =~ /^([^_]+)_([\w\d]+)_(\d*\.?\d*)_(\d+)(?<start_cond>F?)_(\d+)(?<expiry_cond>[FT]?)_(S?-?\d+P?)_(S?-?\d+P?)(_*)(\d*\.?\d*)$/)
     {                               # Both purchase and expiry date are timestamp (e.g. a 30-min bet)
         $bet_type          = $1;
         $underlying_symbol = $2;
@@ -178,6 +178,7 @@ sub shortcode_to_parameters {
         } else {
             $date_expiry = $6;
         }
+        $contract_multiplier = $11;
     } elsif ($shortcode =~ /^([^_]+)_(R?_?[^_\W]+)_(\d*\.?\d*)_(\d+)_(\d+)(?<expiry_cond>[T]?)$/) {    # Contract without barrier
         $bet_type          = $1;
         $underlying_symbol = $2;
