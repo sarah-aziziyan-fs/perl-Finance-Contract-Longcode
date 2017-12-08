6b0de99567cbefddcf23eeb65d7cb0668a093d78
Jun Bon <junbon@binary.com>
2017-12-08 03:01:10 +0000

    fixes

--- lib/Finance/Contract/Longcode.pm
+++ lib/Finance/Contract/Longcode.pm
@@ -167,7 +167,7 @@ sub shortcode_to_parameters {
     return $legacy_params if (not exists Finance::Contract::Category::get_all_contract_types()->{$initial_bet_type} or $shortcode =~ /_\d+H\d+/);
 
     if ($shortcode =~
-        /^([^_]+)_([\w\d]+)_(\d*\.?\d*)_(\d+)(?<start_cond>[F]?)_(\d+)(?<expiry_cond>[FT]?)_(S?-?\d+P?)_(S?-?\d+P?)(?:_(?<window_start>\d+))$/)
+        /^([^_]+)_([\w\d]+)_(\d*\.?\d*)_(\d+)(?<start_cond>[F]?)_(\d+)(?<expiry_cond>[FT]?)_(S?-?\d+P?)_(S?-?\d+P?)(?:_(?<window_start>\d+))?$/)
     {                               # Both purchase and expiry date are timestamp (e.g. a 30-min bet)
         $bet_type          = $1;
         $underlying_symbol = $2;
@@ -238,7 +238,7 @@ sub shortcode_to_parameters {
         %barriers,
     };
 
-    if ($product_type eq 'multi_barrier') {
+    if ($product_type && $product_type eq 'multi_barrier') {
         $bet_parameters->{product_type}         = $product_type;
         $bet_parameters->{trading_window_start} = $trading_window_start;
     }
