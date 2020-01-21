4f286e980625fa888160e01a020080ba29048af1
Jun Bon <junbon@binary.com>
2020-01-21 01:42:17 +0000

    update

--- lib/Finance/Contract/Longcode.pm
+++ lib/Finance/Contract/Longcode.pm
@@ -173,9 +173,9 @@ sub shortcode_to_parameters {
     $is_sold //= 0;
 
     my (
-        $bet_type,     $underlying_symbol,    $payout,        $date_start, $date_expiry,
-        $barrier,      $barrier2,             $fixed_expiry,  $duration,   $contract_multiplier,
-        $product_type, $trading_window_start, $selected_tick, $stake,      $deal_cancellation,
+        $bet_type,      $underlying_symbol, $payout,            $date_start,          $date_expiry,  $barrier,
+        $barrier2,      $fixed_expiry,      $duration,          $contract_multiplier, $product_type, $trading_window_start,
+        $selected_tick, $stake,             $deal_cancellation, $deal_cancellation_tp
     );
 
     my $forward_start = 0;
@@ -189,14 +189,15 @@ sub shortcode_to_parameters {
 
     return $legacy_params if (not exists Finance::Contract::Category::get_all_contract_types()->{$initial_bet_type} or $shortcode =~ /_\d+H\d+/);
 
-    if ($shortcode =~ /^(MULTUP|MULTDOWN)_(R?_?[^_\W]+)_(\d*\.?\d*)_(\d+)_(\d+)_(\d+)_(\d+(?:m|h)?)$/) {
-        $bet_type            = $1;
-        $underlying_symbol   = $2;
-        $stake               = $3;
-        $contract_multiplier = $4;
-        $date_start          = $5;
-        $date_expiry         = $6;
-        $deal_cancellation   = $7;
+    if ($shortcode =~ /^(MULTUP|MULTDOWN)_(R?_?[^_\W]+)_(\d*\.?\d*)_(\d+)_(\d+)_(\d+)_(\d+(?:m|h)?)_(\d+)$/) {
+        $bet_type             = $1;
+        $underlying_symbol    = $2;
+        $stake                = $3;
+        $contract_multiplier  = $4;
+        $date_start           = $5;
+        $date_expiry          = $6;
+        $deal_cancellation    = $7;
+        $deal_cancellation_tp = $8;
     } elsif ($shortcode =~
         /^([^_]+)_([\w\d]+)_(\d*\.?\d*)_(\d+)(?<start_cond>[F]?)_(\d+)(?<expiry_cond>[FT]?)_(S?-?\d+P?)_(S?-?\d+P?)(?:_(?<extra>[PM])(\d*\.?\d+))?$/)
     {    # Both purchase and expiry date are timestamp (e.g. a 30-min bet)
@@ -299,7 +300,8 @@ sub shortcode_to_parameters {
     }
 
     if (defined $deal_cancellation) {
-        $bet_parameters->{deal_cancellation} = $deal_cancellation;
+        $bet_parameters->{deal_cancellation}    = $deal_cancellation;
+        $bet_parameters->{deal_cancellation_tp} = $deal_cancellation_tp;
     }
 
     return $bet_parameters;
